a67e20a6d3a9ee16836734f56fa637cedc648e0a
arushanovanz <nadin.spb@gmail.com>
2022-11-29 21:11:15 +0300

    added gender

--- narti/account/models.py
+++ narti/account/models.py
@@ -8,6 +8,7 @@ class Comics(models.Model):
     photo = models.ImageField(upload_to="photos/%Y/%m/%d")
     is_published = models.BooleanField(default=True)
     language = models.ForeignKey('Language', on_delete=models.PROTECT, null=True)
+    gender = models.CharField(max_length=1, null=True)
 
     def __str__(self):
         return self.title
